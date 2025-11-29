09200c49985febd1b1bbb79c7f7446d5929b702e
Sebastian Lopez-Cot <sebastian.lopez.cot@gmail.com>
2025-11-29 00:38:32 -0500

    Default dual view

---
+++ default_dual_view.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make
+
+./dual_fisheye_viewer ~/kitti360/data/fisheye/fisheye_images/2013_05_28_drive_0000_sync/image_02/data_rgb/ ~/kitti360/data/fisheye/fisheye_images/2013_05_28_drive_0000_sync/image_03/data_rgb/
